b3734595409839957e67803ff5938b63898c46ac
Karen Kuek <karenkk75@gmail.com>
2018-10-23 23:05:19 +1100

    skeleton model completed

--- main.rb
+++ main.rb
@@ -3,13 +3,102 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pg'
 
+require_relative 'db_config'
+require_relative 'models/show'
+require_relative 'models/comment'
+require_relative 'models/like'
+require_relative 'models/user'
+require_relative 'models/suggestion'
 
+enable :sessions
+
+helpers do
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+end
 
 get '/' do
+  @shows = Show.all
   erb :index
 end
 
+get '/shows/:id' do
+  @show = Show.find(params[:id])
+  @comments = @show.comments
+  erb :show
+end
 
+get '/login' do
+  erb :login
+end
 
+get '/suggestions' do
+  erb :suggestion
+end
 
+get '/new' do
+  erb :new
+end
 
+post '/shows' do
+  show = Show.new
+  show.title = params[:title]
+  show.year = params[:year]
+  show.image_url = params[:image_url]
+  show.body = params[:body]
+  show.save
+end
+
+post '/likes' do
+  redirect to('/login') unless logged_in?
+  like = Like.new
+  like.show_id = params[:show_id]
+  like.user_id = params[:user_id]
+  like.save
+  redirect to("/shows/#{params[:show_id]}")
+end
+
+post '/suggestions' do
+  redirect to('/login') unless logged_in?
+  suggestion = Suggestion.new
+  suggestion.user_id = params[:user_id]
+  suggestion.save
+  redirect to('/')
+end
+
+post '/comments' do
+  redirect to('/login') unless logged_in?
+  comment = Comment.new
+  comment.show_id = params[:show_id]
+  comment.user_id = params[:user_id]
+  comment.save
+  redirect to("/shows/#{params[:show_id]}")
+end
+
+delete '/comments/:id' do
+  comment = Comment.find(params[:id])
+  comment.destroy
+  redirect to("/shows/#{comment[:show_id]}")
+end
+
+post '/session' do
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password]) 
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    erb :login
+  end
+end
+
+delete '/session' do
+  session[:user_id] = nil
+  redirect to('/login')
+end
--- shows.sql
+++ shows.sql
@@ -3,6 +3,7 @@ CREATE DATABASE shows_db;
 CREATE TABLE shows(
   id SERIAL PRIMARY KEY,
   title VARCHAR(200),
+  year VARCHAR(100),
   image_url TEXT,
   body TEXT
 );
@@ -10,34 +11,43 @@ CREATE TABLE shows(
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
   email VARCHAR(400),
-  password_digest VARCHAR(400),
-
+  password_digest VARCHAR(400)
 );
 
 CREATE TABLE comments(
   id SERIAL PRIMARY KEY,
   body TEXT,
   show_id INTEGER,
-  FOREIGN KEY (show_id) REFERENCES shows (id) ON DELETE RESTRICT
+  user_id INTEGER,
+  FOREIGN KEY (show_id) REFERENCES shows (id) ON DELETE RESTRICT,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE RESTRICT
+);
+--  
+CREATE TABLE likes(
+  id SERIAL PRIMARY KEY,
+  show_id INTEGER,
+  user_id INTEGER,
+  FOREIGN KEY (show_id) REFERENCES shows (id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE TABLE suggestions(
   id SERIAL PRIMARY KEY,
-  body TEXT,
-  show_id INTEGER
-)
+  user_id INTEGER,
+  body TEXT
+);
 
-INSERT INTO shows (title, image_url) VALUES ('agents of shield', 'https://m.media-amazon.com/images/M/MV5BMTc5NzEzMzA2MF5BMl5BanBnXkFtZTgwNDkyNzgyNDM@._V1_SX300.jpg');
+INSERT INTO shows (title, year, image_url, body) VALUES ('agents of shield', '2013-','https://m.media-amazon.com/images/M/MV5BMTc5NzEzMzA2MF5BMl5BanBnXkFtZTgwNDkyNzgyNDM@._V1_SX300.jpg', 'The missions of the Strategic Homeland Intervention, Enforcement and Logistics Division.');
 
-INSERT INTO shows (title, image_url) VALUES ('arrow', 'https://m.media-amazon.com/images/M/MV5BMTU5MjU5NjUyOV5BMl5BanBnXkFtZTgwMDY1ODIyNjM@._V1_SX300.jpg');
+INSERT INTO shows (title, year, image_url, body) VALUES ('arrow', '2012-','https://m.media-amazon.com/images/M/MV5BMTU5MjU5NjUyOV5BMl5BanBnXkFtZTgwMDY1ODIyNjM@._V1_SX300.jpg', 'Spoiled billionaire playboy Oliver Queen is missing and presumed dead when his yacht is lost at sea. He returns five years later a changed man, determined to clean up the city as a hooded vigilante armed with a bow.');
 
-INSERT INTO shows (title, image_url) VALUES ('black lightning', 'https://m.media-amazon.com/images/M/MV5BMTY3NTk5Mzg5OV5BMl5BanBnXkFtZTgwNDgyMjQyNjM@._V1_SX300.jpg');
+INSERT INTO shows (title, year, image_url, body) VALUES ('black lightning', '2018','https://m.media-amazon.com/images/M/MV5BMTY3NTk5Mzg5OV5BMl5BanBnXkFtZTgwNDgyMjQyNjM@._V1_SX300.jpg', 'A crusading school principal gets back into action as the original African-American electrical superhero.');
 
-INSERT INTO shows (title, image_url) VALUES ('daredevil', 'https://m.media-amazon.com/images/M/MV5BODcwOTg2MDE3NF5BMl5BanBnXkFtZTgwNTUyNTY1NjM@._V1_SX300.jpg');
+INSERT INTO shows (title, year, image_url, body) VALUES ('daredevil', '2015', 'https://m.media-amazon.com/images/M/MV5BODcwOTg2MDE3NF5BMl5BanBnXkFtZTgwNTUyNTY1NjM@._V1_SX300.jpg', 'A blind lawyer by day, vigilante by night. Matt Murdock fights the crime of New York as Daredevil.');
 
-INSERT INTO shows (title, image_url) VALUES ('the flash', 'https://m.media-amazon.com/images/M/MV5BMjI1MDAwNDM4OV5BMl5BanBnXkFtZTgwNjUwNDIxNjM@._V1_SX300.jpg');
+INSERT INTO shows (title, year, image_url, body) VALUES ('the flash', '2014', 'https://m.media-amazon.com/images/M/MV5BMjI1MDAwNDM4OV5BMl5BanBnXkFtZTgwNjUwNDIxNjM@._V1_SX300.jpg', 'After being struck by lightning, Barry Allen wakes up from his coma to discover he has been given the power of super speed, becoming the Flash, fighting crime in Central City.');
 
-INSERT INTO shows (title, image_url) VALUES ('jessica jones', 'https://m.media-amazon.com/images/M/MV5BMTkxMDk5NTQ3MF5BMl5BanBnXkFtZTgwNzg3ODU3NDM@._V1_SX300.jpg');
+INSERT INTO shows (title, year, image_url, body) VALUES ('jessica jones', '2015', 'https://m.media-amazon.com/images/M/MV5BMTkxMDk5NTQ3MF5BMl5BanBnXkFtZTgwNzg3ODU3NDM@._V1_SX300.jpg', 'Following the tragic end of her brief superhero career, Jessica Jones tries to rebuild her life as a private investigator, dealing with cases involving people with remarkable abilities in New York City.');
 
 INSERT INTO shows (title, image_url) VALUES ('legends of tomorrow', 'https://m.media-amazon.com/images/M/MV5BODVjYzliMTQtODk0My00MmUxLTkwMzMtMWJjMGQzZmU5MzAwXkEyXkFqcGdeQXVyNTc4MjczMTM@._V1_SX300.jpg');
 
