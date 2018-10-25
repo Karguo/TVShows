47ca970e00d6a0ea41ebe7a86b6a2d6c5284013f
Karen Kuek <karenkk75@gmail.com>
2018-10-26 10:17:39 +1100

    working

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,11 @@
-function switchTheme(sheet) {
-  document.getElementById('switchtheme').setAttribute('href', sheet);
+function setStyleSheet(sheet) {
+  var setStyleSheet = document.getElementById('switchtheme');
+  setStyleSheet.setAttribute('href', sheet);
+  localStorage.setItem('theme', `${sheet}`)
 }
+
+let localTheme = localStorage.getItem('theme')
+
+if (localStorage.getItem('theme')) {
+  setStyleSheet(localTheme)
+};
